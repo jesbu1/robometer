496898069d74af15e5b7a204c6eca75809ad63ab
Jesse Zhang <jessezha@cs.washington.edu>
2026-03-12 17:05:21 -0700

    remove safetensors arg for transformers 5.0 and then update the config for wandb name and dataset_category for correct roboreward names

--- robometer/utils/setup_utils.py
+++ robometer/utils/setup_utils.py
@@ -1090,7 +1090,6 @@ def create_training_arguments(cfg: TrainingConfig, output_dir: str, is_eval: boo
         "dataloader_pin_memory": cfg.dataloader_pin_memory,
         "dataloader_num_workers": cfg.dataloader_num_workers,
         "dataloader_persistent_workers": cfg.dataloader_persistent_workers,
-        "save_safetensors": True,
         "save_total_limit": 2,
         # Evaluation settings
         "eval_strategy": cfg.evaluation_strategy,
